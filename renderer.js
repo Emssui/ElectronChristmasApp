071af9da22eb7c319bd0a443a10a0c66fa5c23b5
Emssui <eames.moller@gmail.com>
2024-11-06 09:25:03 +0200

    Add menu icon

--- renderer.js
+++ renderer.js
@@ -1,20 +1,16 @@
-// renderer.js
 const body = document.body;
 const container = document.createElement("div");
 
-// Create the close button image
 const closeImg = document.createElement("img");
-closeImg.src = "assets/images/close.png"; // Use your close image here
+closeImg.src = "assets/images/close.png"; 
 closeImg.alt = "Close";
-closeImg.id = "closeBtn"; // Set the ID to match the event listener
-closeImg.style.padding = "10px"; // Add padding
-closeImg.style.cursor = "pointer"; // Change cursor on hover
+closeImg.id = "closeBtn";
+closeImg.style.padding = "10px"; 
+closeImg.style.cursor = "pointer"; 
 
-// Append the close image to the container
 container.appendChild(closeImg);
 body.appendChild(container);
 
-// Add click event listener to close the window
 closeImg.addEventListener("click", () => {
-    window.electron.closeApp(); // Call the exposed closeApp method from preload
+    window.electron.closeApp(); 
 });
